f90a57b4017d4cba31a479bf8aeab002d2f222b0
unknown <bigjolemaire@gmail.com>
2025-03-16 15:13:08 +0000

    current state of pages

--- views/student-dashboard.php
+++ views/student-dashboard.php
@@ -3,16 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Dashboard - Student</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/general-styles.css">
+    <link rel="stylesheet" href="../assets/css/dashboard-styles.css">
 </head>
 
 <body class="bg-light">
-    <div class="container d-flex justify-content-center align-items-center vh-100">
-        <div class="card p-4 shadow-lg">
+    <div class="container-fluid d-flex justify-content-center align-items-center vh-100">
+        <div class="card p-4 shadow-lg main">
 
+        <!-- Navbar -->
+		<nav class = 'navbar navbar-expand-sm bg-primary rounded'>
+            <div class = 'container-fluid'>
+                <ul class = 'navbar-nav'>
+                    <li class = 'nav-item'>
+                        <a href = '#' class = 'nav-link text-light'> Home </a>
+                    </li>
+                    <li class = 'nav-item'>
+                        <a href = '#' class = 'nav-link text-light'> Events </a>
+                    </li>
+                    <li class = 'nav-item'>
+                        <a href = '#' class = 'nav-link text-light'> Search </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- Body -->
+            <h1> Hello <!-- User's first and last name output here --> </h1>
         </div>
     </div>
 </body>
