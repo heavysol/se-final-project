7d2a9348ecefc690e158c81c3834e8c15114f7cd
unknown <bigjolemaire@gmail.com>
2025-03-19 16:28:42 +0000

    views: linked dashboards to dashboard styling file

--- views/organizer_dashboard.php
+++ views/organizer_dashboard.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Event Organizer Dashboard - Campus Events</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        
-</style>
+    <link rel="stylesheet" href="../assets/css/general-styles.css">
+    <link rel="stylesheet" href="../assets/css/dashboard-styles.css">
 </head>
 <body>
 <!-- Sidebar -->
