dfe7737b4d66f63c4e7e0d8134deb7226d1a52ee
NsabimanaSteve <steve.nsabimana@ashesi.edu.gh>
2025-04-25 17:17:27 +0000

    trying to make changes to the signup page

--- views/Signup.php
+++ views/Signup.php
@@ -74,7 +74,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up | FitInspire Hub</title>
+    <title>Sign Up | Campus Events</title>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
